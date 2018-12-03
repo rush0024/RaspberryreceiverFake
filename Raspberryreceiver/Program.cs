5cd26bf649716f416752581f9b45ec10a813c127
Rushika Tamrakar <Rushika Tamrakar@LAPTOP-D490EOAI>
2018-12-03 13:57:40 +0100

    raspberrysensorfake

--- Raspberryreceiver/Program.cs
+++ Raspberryreceiver/Program.cs
@@ -1,21 +1,95 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Raspberryreceiver.model;
 
-namespace Raspberryreceiver
+namespace BerthAppRaspberrySensorReceiver
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            double pressure;
+            double temperature;
+            double humidity;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            int number = 0;
+
+            //Creates a UdpClient for reading incoming data.
+
+            UdpClient udpReceiver = new UdpClient();
+
+            // This IPEndPoint will allow you to read datagrams sent from any ip-source on port 6969
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 6969);
+
+            Console.WriteLine("Receiver is blocked");
+
+            try
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("Sender: " + receivedData.ToString());
+
+                    string[] textLines = receivedData.Split('\n');
+
+                    for (int index = 0; index < textLines.Length; index++)
+                        Console.Write(textLines[index]);
+
+                    string[] list1 = textLines[0].Split(':');
+                    string text1 = list1[1];
+                    string[] list2 = textLines[1].Split(':');
+                    string text2 = list2[1];
+                    string[] list3 = textLines[2].Split(':');
+                    string text3 = list3[1];
+
+                    humidity = Double.Parse(text1);
+                    pressure = Double.Parse(text2);
+                    temperature = Double.Parse(text3);
+
+                    Console.WriteLine("Long: " + humidity);
+                    Console.WriteLine("Lat: " + pressure);
+                    Console.WriteLine("BpSystolic: " + temperature);
+
+                    Record record = new Record(pressure, temperature, humidity);
+
+                    PostRecordAsync(record);
+
+                    //Console.ReadLine(); //for reading the data slowly
+                    Thread.Sleep(1000);
+
+                    number++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static async Task PostRecordAsync(Record record)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://berthapibeta20181025031131.azurewebsites.net");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/records", record);
+            response.EnsureSuccessStatusCode();
+
+            Console.Write("Este es el status actual: " + response);
         }
     }
 }
---
+++ Raspberryreceiver/model/Record.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raspberryreceiver.model
+{
+   
+        public class Record
+        {
+            public double Pressure { get; set; }
+
+            public double Temperature { get; set; }
+
+            public double Humidity { get; set; }
+
+            public Record(double pressure, double temperature, double humidity)
+            {
+                Humidity = humidity;
+                Pressure = pressure;
+                Temperature = temperature;
+            }
+        }
+    }
+}
